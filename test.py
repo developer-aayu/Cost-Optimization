e0a6f0b27a9658ab70e9af5e5fbb2d61dd9a04c1
developer-aayu <aayusharma00123@gmail.com>
2024-08-28 07:59:03 +0530

    test.py added

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import boto3
 client = boto3.client('s3')         #use the client to specify the service used
 
 response = client.delete_bucket(
-    Bucket='aayu-test-bucket-1409',
-    ExpectedBucketOwner='975050175364'
+    Bucket='aayu-test-bucket-1409'
 )
 
 print(response)
