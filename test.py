91e4393d78d23491e555def5a5aa52158013a71c
developer-aayu <aayusharma00123@gmail.com>
2024-08-28 20:57:32 +0530

    stale ebs snapshot delete file created

--- test.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-client = boto3.client('s3')         #use the client to specify the service used
-
-response = client.delete_bucket(
-    Bucket='aayu-test-bucket-1409'
-)
-
-print(response)
